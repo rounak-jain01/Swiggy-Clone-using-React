6467a149e625bb7b725ae19c72027c410f9fedc8
Rounak Jain <rounakjain049@gmail.com>
2025-09-30 22:17:39 +0530

    Start Restaurant

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Dishes from "./Dishes";
 import TopRestaurants from "./TopRestaurants";
 
 function Body() {
+  const [DishesData, setDishesData] = useState([]);
+
+  const [restaurantData, setRestaurantData] = useState([]);
+
+  async function fetchData() {
+    const response = await fetch(
+      "/api/dapi/restaurants/list/v5?lat=12.97530&lng=77.59100&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const result = await response.json();
+    console.log(
+      result?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setDishesData(result?.data?.cards[0]?.card?.card?.imageGridCards?.info);
+
+    setRestaurantData(
+      result?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="w-full">
-      <div className="w-[80%] p-2 px-4 mx-auto overflow-hidden gap-20">
-        <Dishes />
-        <TopRestaurants/>
+    <div className="w-full h-screen">
+      <div className="w-[80%] h-[100%] p-2 px-4 mx-auto overflow-hidden gap-20">
+        <Dishes Data={DishesData} />
+        <TopRestaurants data={restaurantData} />
       </div>
     </div>
   );
